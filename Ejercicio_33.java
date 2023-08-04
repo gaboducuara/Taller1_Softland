ae54004111a2aeaea40955f1d67e0ef23773fc1f
Gabriel <gabrielmancilladucuara@gmail.com>
2023-08-04 14:08:32 -0500

    realizando los ejercicios los que entendi

--- Ejercicio_33.java
+++ Ejercicio_33.java
@@ -1,4 +1,6 @@
-//Frecuencia de elementos:
-//Escribe un programa que cuente la frecuencia de cada elemento en un arreglo.
+//Frecuencia de elementos: Escribe un programa que cuente la frecuencia de cada
+// elemento en un arreglo.
 public class Ejercicio_33 {
+    //no entendi el ejercicio
+
 }
